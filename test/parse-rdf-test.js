faa13466b65b10da8740145ab757ded6c116e4ec
Joel Palmer <joelpalmer@gmail.com>
2018-12-15 10:38:25 -0600

    BDD: parseRDF test harness in place

--- test/parse-rdf-test.js
+++ test/parse-rdf-test.js
@@ -1,7 +1,7 @@
 "use strict";
 const fs = require("fs");
 const expect = require("chai").expect;
-
+const parseRDF = require('../lib/parse-rdf');
 const rdf = fs.readFileSync(`${__dirname}/pg132.rdf`);
 
 describe('parseRDF', ()=> {
