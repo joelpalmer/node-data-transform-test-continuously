15337d1a62269bf409e339820335aa2479ea7c11
Joel Palmer <joelpalmer@gmail.com>
2018-12-15 13:11:03 -0600

    finish bulk prep for elasticsearch import

--- rdf-to-bulk.js
+++ rdf-to-bulk.js
@@ -1,2 +1,20 @@
 "use strict";
 const dir = require('node-dir');
+const parseRDF = require('./lib/parse-rdf');
+
+const dirName = process.argv[2];
+
+const options = {
+    match: /\.rdf$/, // match rdf files
+    exclude: ['pg0.rdf'] // ignore the template
+};
+
+dir.readFiles(dirName, options, (err, content, next) => {
+    if (err) throw err;
+    const doc = parseRDF(content);
+    console.log(JSON.stringify({
+        index: {_id: `pq${doc.id}`}
+    }));
+    console.log(JSON.stringify(doc));
+    next();
+});
